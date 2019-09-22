d5620461bd9e651bf05061bed93193b356ffe02e
skyland31 <suttipong.kay.10@gmail.com>
2019-09-22 11:57:54 +0700

    chang test and maha-lab

--- maha-lap.js
+++ maha-lap.js
@@ -1,6 +1,54 @@
 class MahaLap {
   static tamnaiWithDay(day) {
-    return 2;
+    switch (true) {
+      case day === "จันทร์":
+        return 2;
+      case day === "อังคาร":
+        return 3;
+      case day === "พุธ":
+        return 4;
+      case day === "พฤหัสบดี":
+        return 5;
+      case day === "ศุกร์":
+        return 6;
+      case day === "เสาร์":
+        return 7;
+      case day === "อาทิตย์":
+        return 1;
+      case day === "สงกรานต์":
+        return 13;
+      case day === "ฮาโลวีน":
+        return 31;
+      case day === "คริสมาสต์":
+        return 25;
+      default:
+        return "Error";
+    }
+  }
+  static tamnaiWithStory(story) {
+    const length = story.length;
+    var sum = 0;
+    for (let i = 0; i <= length; i++) {
+      sum = sum + i;
+    }
+    sum = sum % 100;
+    return sum;
+  }
+  static tamnaiWithTime(time) {
+    const randomNumber = Math.random();
+    if (time === "กลางวัน") {
+      if (randomNumber >= 0.5) {
+        return randomNumber;
+      } else {
+        return 0.5;
+      }
+    } else if (time === "กลางคืน") {
+      if (randomNumber < 0.5) {
+        return randomNumber;
+      } else {
+        return 0.5;
+      }
+    }
   }
 }
 module.exports = MahaLap;
--- maha-lap.test.js
+++ maha-lap.test.js
@@ -1,7 +1,79 @@
 const MahaLab = require("./maha-lap");
 describe("Test Maha-Lap", () => {
-  test("Test Tamai with day", () => {
+  test("Test Tamai with day return 2", () => {
     const result = MahaLab.tamnaiWithDay("จันทร์");
     expect(result).toEqual(2);
+  });
+  test("Test Tamai with day return 3", () => {
+    const result = MahaLab.tamnaiWithDay("อังคาร");
+    expect(result).toEqual(3);
+  });
+  test("Test Tamai with day return 4", () => {
+    const result = MahaLab.tamnaiWithDay("พุธ");
+    expect(result).toEqual(4);
+  });
+  test("Test Tamai with day return 5", () => {
+    const result = MahaLab.tamnaiWithDay("พฤหัสบดี");
+    expect(result).toEqual(5);
+  });
+  test("Test Tamai with day return 6", () => {
+    const result = MahaLab.tamnaiWithDay("ศุกร์");
+    expect(result).toEqual(6);
+  });
+  test("Test Tamai with day return 7", () => {
+    const result = MahaLab.tamnaiWithDay("เสาร์");
+    expect(result).toEqual(7);
+  });
+  test("Test Tamai with day return 1", () => {
+    const result = MahaLab.tamnaiWithDay("อาทิตย์");
+    expect(result).toEqual(1);
+  });
+  test("Test Tamai with day return 13", () => {
+    const result = MahaLab.tamnaiWithDay("สงกรานต์");
+    expect(result).toEqual(13);
+  });
+  test("Test Tamai with day return 31", () => {
+    const result = MahaLab.tamnaiWithDay("ฮาโลวีน");
+    expect(result).toEqual(31);
+  });
+  test("Test Tamai with day return 25", () => {
+    const result = MahaLab.tamnaiWithDay("คริสมาสต์");
+    expect(result).toEqual(25);
+  });
+  test("Test Tamai with day return Error", () => {
+    const result = MahaLab.tamnaiWithDay("ปีใหม่");
+    expect(result).toEqual("Error");
+  });
+  test("Test Tamai with story งู 3 ", () => {
+    const result = MahaLab.tamnaiWithStory("งู");
+    expect(result).toEqual(3);
+  });
+  test("Test Tamai with story แมว 6 ", () => {
+    const result = MahaLab.tamnaiWithStory("แมว");
+    expect(result).toEqual(6);
+  });
+  test("Test Tamai with story cat and dog ", () => {
+    const result = MahaLab.tamnaiWithStory("cat and dog");
+    expect(result).toEqual(66);
+  });
+  test("Test Tamai with time กลางวัน random = 0.98 return 0.98", () => {
+    global.Math.random = jest.fn(() => 0.98);
+    const result = MahaLab.tamnaiWithTime("กลางวัน");
+    expect(result).toEqual(0.98);
+  });
+  test("Test Tamai with time กลางวัน random = 0.4 return 0.5", () => {
+    global.Math.random = jest.fn(() => 0.4);
+    const result = MahaLab.tamnaiWithTime("กลางวัน");
+    expect(result).toEqual(0.5);
+  });
+  test("Test Tamai with time กลางคืน random = 0.98 return 0.98", () => {
+    global.Math.random = jest.fn(() => 0.98);
+    const result = MahaLab.tamnaiWithTime("กลางคืน");
+    expect(result).toEqual(0.5);
+  });
+  test("Test Tamai with time กลางคืน random = 0.4 return 0.5", () => {
+    global.Math.random = jest.fn(() => 0.4);
+    const result = MahaLab.tamnaiWithTime("กลางคืน");
+    expect(result).toEqual(0.4);
   });
 });
